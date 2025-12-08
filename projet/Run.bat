ce93edf1a850c655f3a46487ed7e3ababa005c04
DNB_Dono0530 <donovan1430@gmail.com>
2025-12-08 13:06:27 +0100

    Ajouts Des Run
    ADD : - Run.bat
               - Run.sh

---
+++ projet/Run.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Compilation en cours...
+javac "@compile.list" -d class
+if %errorlevel% neq 0 (
+    echo Erreur de compilation!
+    pause
+    exit /b %errorlevel%
+)
+echo Compilation reussie!
+echo.
+echo Execution du programme...
+cd class
+java projet.Controleur
+pause
---
+++ projet/Run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Compilation en cours..."
+javac "@compile.list" -d class
+if [ $? -ne 0 ]; then
+    echo "Erreur de compilation!"
+    exit 1
+fi
+echo "Compilation reussie!"
+echo
+echo "Execution du programme..."
+cd class
+java projet.Controleur
