3f12e2c2010ac557512e25dd908079bc2e0e85aa
mioko3 <erwan.martin30@outlook.fr>
2025-12-08 14:45:26 +0100

    class LectureRepertoire

---
+++ projet/metier/LectureRepertoire.java
@@ -0,0 +1,32 @@
+package projet.metier;
+
+import java.io.File;
+import java.util.List;
+
+
+public class LectureRepertoire
+{
+	private List<CreeClass> lstClass;
+
+	public LectureRepertoire(File rep)
+	{
+		File[] fichiers = rep.listFiles();
+
+		if (fichiers != null)
+		{
+			for (File f : fichiers)
+			{
+				if (f.getName().endsWith(".java"))
+				{
+					CreeClass c = CreeClass.factoryCreeClass(f.getPath());
+					if (c != null)
+					{
+						this.lstClass.add(c);
+					}
+				}
+			}
+		}
+
+	}
+
+}
