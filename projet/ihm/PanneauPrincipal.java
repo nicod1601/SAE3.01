3f759c8c0bcf1eda38711181515c4e59502f9945
nicod1601 <nicod162005@gmail.com>
2025-12-10 12:24:04 +0100

    dessin

--- projet/ihm/PanneauPrincipal.java
+++ projet/ihm/PanneauPrincipal.java
@@ -1,5 +1,7 @@
 package projet.ihm;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JPanel;
@@ -8,37 +10,33 @@ import projet.metier.CreeClass;
 
 public class PanneauPrincipal extends JPanel 
 {
-
     private Controleur ctrl;
     private List<CreeClass> lstClass;
     private List<JPanel> lstPanel;
 
-
     public PanneauPrincipal(Controleur ctrl) 
     {
+        this.setLayout(new FlowLayout());
         this.ctrl = ctrl;
-        this.lstClass = new ArrayList<CreeClass>();
-        this.lstPanel = new ArrayList<JPanel>();
+        this.lstClass = new ArrayList<>();
+        this.lstPanel = new ArrayList<>();
     }
 
-    public void majListeClasses(boolean dossier , String nomFichier)
+    public void majListeClasses(boolean dossier, String nomFichier)
     {
-        if (dossier)
+        if (dossier) 
         {
             this.lstClass = this.ctrl.getLstClass();
-
-            for (CreeClass classe : this.lstClass)
+            for (CreeClass classe : this.lstClass) 
             {
                 System.out.println(classe.getNom());
             }
-        }
+        } 
         else
         {
             System.out.println("Nom Fichier reçu : " + nomFichier);
-
             CreeClass nouvelleClasse = this.ctrl.CreerClass(nomFichier);
-
-            // Vérifier si elle existe déjà
+            
             for (CreeClass classe : this.lstClass)
             {
                 if (classe.getNom().equals(nouvelleClasse.getNom()))
@@ -47,27 +45,50 @@ public class PanneauPrincipal extends JPanel
                     return;
                 }
             }
-
             this.lstClass.add(nouvelleClasse);
-
             for (CreeClass classe : this.lstClass)
             {
                 System.out.println(classe.getNom());
             }
         }
+        this.dessinerUML();
     }
-
 
     public void viderListeClasses()
     {
         this.lstClass.clear();
+        this.lstPanel.clear();
+        this.removeAll();
+        this.revalidate();
+        this.repaint();
     }
 
     public void dessinerUML()
     {
-        for(int cpt = 0; cpt < this.lstClass.size(); cpt++)
+        this.lstPanel.clear();
+        this.removeAll();
+        
+        for (CreeClass classe : this.lstClass) 
         {
-            this.lstPanel.add(this.ctrl.UML(this.lstClass.get(cpt)));
+            this.lstPanel.add(this.ctrl.UML(classe));
         }
+
+        for(JPanel p : this.lstPanel)
+        {
+            p.setPreferredSize(new Dimension(500, 500));
+        }
+        
+        this.posComposant();
+    }
+
+    public void posComposant() 
+    {
+        for (JPanel panel : this.lstPanel)
+        {
+            this.add(panel);
+        }
+        
+        this.revalidate();
+        this.repaint();
     }
 }
