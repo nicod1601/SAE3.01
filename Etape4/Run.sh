97c8d75455900e16d1194bb3ececd17c2e92cb92
DNB_Dono0530 <donovan1430@gmail.com>
2025-12-12 11:40:21 +0100

    Update Run.sh

--- Etape4/Run.sh
+++ Etape4/Run.sh
@@ -2,8 +2,8 @@
 echo "Compilation en cours..."
 javac @compile.list -d class
 if [ $? -ne 0 ]; then
-    echo "Erreur de compilation!"
-    exit 1
+   echo "Erreur de compilation!"
+   exit 1
 fi
 echo "Compilation reussie!"
 echo
